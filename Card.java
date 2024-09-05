f34c3f39b798f9c3f730bfc5f9a0245180cc9a41
LuisF1238 <168211150+LuisF1238@users.noreply.github.com>
2024-09-05 22:02:26 +0000

    upload

---
+++ Card.java
@@ -0,0 +1,28 @@
+public class Card {
+
+   private String suit;
+    private int value;
+    private String rank;
+
+    public Card() {
+        
+        this.value = 0;
+        this.suit = "no suit";
+        this.rank = "No rank";
+    }
+
+
+    public Card (int value, String suit, String rank){
+
+        
+
+
+
+    }
+
+    
+
+
+
+
+}
